df264ee8a8d6e60683532e0d059a86e542f1af45
DESKTOP-4C24SK7\Admin <kirylshreyter@gmail.com>
2016-10-23 14:46:08 +0300

    no message

--- db/hotel_mysql_create.sql
+++ db/hotel_mysql_create.sql
@@ -8,7 +8,7 @@ CREATE TABLE `client` (
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `application` (
+CREATE TABLE `booking_request` (
 	`id` bigint NOT NULL,
 	`client_id` bigint NOT NULL,
 	`room_details_id` bigint NOT NULL,
@@ -23,14 +23,16 @@ CREATE TABLE `employee` (
 	`last_name` varchar(256) NOT NULL,
 	`phone` varchar(16) NOT NULL,
 	`email` varchar(128) NOT NULL,
+	`position` varchar(16) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `room_order` (
 	`id` bigint NOT NULL,
 	`room_id` bigint NOT NULL,
-	`application_id` bigint NOT NULL,
+	`booking_request_id` bigint NOT NULL,
 	`employee_id` bigint NOT NULL,
+	`total_cost` DECIMAL NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -41,34 +43,37 @@ CREATE TABLE `room` (
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `occupied_room_for_a_while` (
+CREATE TABLE `booked_room` (
 	`id` bigint NOT NULL,
-	`application_id` bigint NOT NULL,
-	`room_id` bigint NOT NULL,
+	`booking_request_id` bigint NOT NULL,
+	`room_order_id` bigint NOT NULL,
+	`booked_start_day` DATE NOT NULL,
+	`booked_end_day` DATE NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `room_details` (
 	`id` bigint NOT NULL,
 	`number_of_places` INT(8) NOT NULL,
-	`apartments_class` varchar(24) NOT NULL,
-	`cost_per_day` DECIMAL NOT NULL,
+	`cost_per_night` DECIMAL NOT NULL,
+	`room_type` varchar(24) NOT NULL,
+	`floor` varchar(4) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `application` ADD CONSTRAINT `application_fk0` FOREIGN KEY (`client_id`) REFERENCES `client`(`id`);
+ALTER TABLE `booking_request` ADD CONSTRAINT `booking_request_fk0` FOREIGN KEY (`client_id`) REFERENCES `client`(`id`);
 
-ALTER TABLE `application` ADD CONSTRAINT `application_fk1` FOREIGN KEY (`room_details_id`) REFERENCES `room_details`(`id`);
+ALTER TABLE `booking_request` ADD CONSTRAINT `booking_request_fk1` FOREIGN KEY (`room_details_id`) REFERENCES `room_details`(`id`);
 
 ALTER TABLE `room_order` ADD CONSTRAINT `room_order_fk0` FOREIGN KEY (`room_id`) REFERENCES `room`(`id`);
 
-ALTER TABLE `room_order` ADD CONSTRAINT `room_order_fk1` FOREIGN KEY (`application_id`) REFERENCES `application`(`id`);
+ALTER TABLE `room_order` ADD CONSTRAINT `room_order_fk1` FOREIGN KEY (`booking_request_id`) REFERENCES `booking_request`(`id`);
 
 ALTER TABLE `room_order` ADD CONSTRAINT `room_order_fk2` FOREIGN KEY (`employee_id`) REFERENCES `employee`(`id`);
 
 ALTER TABLE `room` ADD CONSTRAINT `room_fk0` FOREIGN KEY (`room_details_id`) REFERENCES `room_details`(`id`);
 
-ALTER TABLE `occupied_room_for_a_while` ADD CONSTRAINT `occupied_room_for_a_while_fk0` FOREIGN KEY (`application_id`) REFERENCES `application`(`id`);
+ALTER TABLE `booked_room` ADD CONSTRAINT `booked_room_fk0` FOREIGN KEY (`booking_request_id`) REFERENCES `booking_request`(`id`);
 
-ALTER TABLE `occupied_room_for_a_while` ADD CONSTRAINT `occupied_room_for_a_while_fk1` FOREIGN KEY (`room_id`) REFERENCES `room_order`(`room_id`);
+ALTER TABLE `booked_room` ADD CONSTRAINT `booked_room_fk1` FOREIGN KEY (`room_order_id`) REFERENCES `room_order`(`id`);
 
